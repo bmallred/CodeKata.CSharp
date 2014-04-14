dd439522148681074081eaa5c48475ec7d769a37
Bryan Allred <bryan.allred@gmail.com>
2014-04-14 08:31:16 -0400

    Added KarateChop Kata

---
+++ CodeKata/Katas/KarateChop.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeKata.Katas
+{
+    /// <summary>
+    /// Karate Chop.
+    /// </summary>
+    /// <remarks>
+    /// Implement a binary search routine (using the specification below) in the language and technique of your choice.
+    /// Tomorrow, implement it again, using a totally different technique.
+    /// Do the same the next day, until you have five totally unique implementations of a binary chop.
+    /// 
+    /// (For example, one solution might be the traditional iterative approach, one might be recursive,
+    /// one might use a functional style passing array slices around, and so on).
+    /// </remarks>
+    public class KarateChop
+    {
+        /// <summary>
+        /// Chops the specified target.
+        /// </summary>
+        /// <param name="target">The target.</param>
+        /// <param name="array">The array.</param>
+        /// <returns>The index of the targetted number (-1 if not found).</returns>
+        public static int Chop(int target, int[] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeKataTests/Solutions/KarateChopTests.cs
@@ -0,0 +1,63 @@
+﻿using CodeKata.Katas;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeKataTests.Solutions
+{
+    /// <summary>
+    /// Karata Chop Tests.
+    /// </summary>
+    [TestClass]
+    public class KarateChopTests
+    {
+        /// <summary>
+        /// Returneds the index of the results are zero.
+        /// </summary>
+        [TestMethod]
+        public void ReturnedResultsAreZeroIndex()
+        {
+            Assert.AreEqual(0, KarateChop.Chop(1, new int[] { 1 }));
+            Assert.AreEqual(0, KarateChop.Chop(1, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(0, KarateChop.Chop(1, new int[] { 1, 3, 5, 7 }));
+        }
+
+        /// <summary>
+        /// Determines whether this instance [can find targets].
+        /// </summary>
+        [TestMethod]
+        public void CanFindTargets()
+        {
+            Assert.AreEqual(1,  KarateChop.Chop(3, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(2,  KarateChop.Chop(5, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(1,  KarateChop.Chop(3, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(2,  KarateChop.Chop(5, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(3,  KarateChop.Chop(7, new int[] { 1, 3, 5, 7 }));
+        }
+
+        /// <summary>
+        /// Determines whether this instance [can handle empty arrays].
+        /// </summary>
+        [TestMethod]
+        public void CanHandleEmptyArrays()
+        {
+            Assert.AreEqual(-1, KarateChop.Chop(3, new int[] { }));
+        }
+
+        /// <summary>
+        /// Determines whether this instance [can identify not found targets].
+        /// </summary>
+        [TestMethod]
+        public void CanIdentifyNotFoundTargets()
+        {
+            Assert.AreEqual(-1, KarateChop.Chop(3, new int[] { 1 }));
+            Assert.AreEqual(-1, KarateChop.Chop(0, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(-1, KarateChop.Chop(2, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(-1, KarateChop.Chop(4, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(-1, KarateChop.Chop(6, new int[] { 1, 3, 5 }));
+            Assert.AreEqual(-1, KarateChop.Chop(0, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, KarateChop.Chop(2, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, KarateChop.Chop(4, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, KarateChop.Chop(6, new int[] { 1, 3, 5, 7 }));
+            Assert.AreEqual(-1, KarateChop.Chop(8, new int[] { 1, 3, 5, 7 }));
+        }
+    }
+}
